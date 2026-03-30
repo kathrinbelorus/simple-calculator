2e932365a4e8d060f9bcd06efca38f6d0e1ce65a
kathrinbelorus <kathrinbelorus@gmail.com>
2026-03-30 14:18:04 +0300

    Created basic calculator functions

---
+++ main.py
@@ -0,0 +1,18 @@
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1/num2
+
+# בדיקה של הפונקציות
+print("--- My Calculator Results ---")
+print("Addition (10 + 5):", add(10, 5))
+print("Subtraction (10 - 5):", sub(10, 5))
+print("Multiplication (10 * 5):", mul(10, 5))
+print("Division (10 / 5):", div(10, 5))
