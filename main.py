aac4ebb59300d3a4aff9bbb0e0a688960df6bbac
kathrinbelorus <kathrinbelorus@gmail.com>
2026-03-31 23:00:27 +0300

    added input check and division by zero

--- main.py
+++ main.py
@@ -10,10 +10,32 @@ def mul(num1, num2):
 def div(num1, num2):
     return num1/num2
 
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+# check if input are numbers
+def is_number(a):
+    try:
+        float(a)
+        return True
+    except ValueError:
+        return False
+     
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
 op = input("Enter an operation you want to perform: ")
+
+try:
+    num1 = float(num1)
+    num2 = float(num2)
+except ValueError: 
+    print('Invalid number')
+    exit()
+
+
+
+if(num2 == 0 and op == '/'):
+    print("It is illegal to divide by 0")
+    exit()
+
+
 
 if(op == '+'):
     result = add(num1, num2)
